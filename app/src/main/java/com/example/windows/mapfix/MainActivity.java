ff25f092d311ceee49effd7834748a0eb389d211
komodoman123 <rifqi546@gmail.com>
2018-02-06 22:01:30 +0700

    map
    -display map
    
    -get device location

---
+++ app/src/main/java/com/example/windows/mapfix/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.windows.mapfix;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG= "MainActivity";
+
+    private static final int ERROR_DIALOG_REQUEST=9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(isServicesok()){
+            init();
+        }
+    }
+
+    private void init(){
+        Button btnMap = (Button) findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    public boolean isServicesok(){
+        Log.d(TAG, "isServicesok: cek versi google service");
+        int versi = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if(versi== ConnectionResult.SUCCESS){
+            //bisa request
+            Log.d(TAG, "isServicesok: google service ok");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(versi)){
+            Log.d(TAG, "isServicesok: error");
+            Dialog dialog=GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,versi,ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this,"cant make map request",Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/com/example/windows/mapfix/MapActivity.java
@@ -0,0 +1,149 @@
+package com.example.windows.mapfix;
+
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.windows.mapfix.R;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+/**
+ * Created by Windows on 06/02/2018.
+ */
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this,"Map Ready",Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
+        Gmap=googleMap;
+
+        if(locPermission){
+            getDeviceLocation();
+            Gmap.setMyLocationEnabled(true);
+
+        }
+    }
+    private static final String TAG="MApActivity";
+
+    private static final String fine_loc="Manifest.permission.ACCESS_FINE_LOCATION";
+    private static final String coarse_loc="Manifest.permission.ACCESS_COARSE_LOCATION";
+    private static final int Location_permission_request_code=1234;
+    private boolean locPermission=false;
+    private GoogleMap Gmap;
+   private FusedLocationProviderClient location_provider;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        getLocationPermission();
+    }
+
+
+
+    private void getDeviceLocation(){
+        Log.d(TAG, "getDeviceLocation: get current location");
+
+        location_provider= LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(locPermission){
+                Task location=location_provider.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: location found");
+                            Location currentLocation=(Location)task.getResult();
+                            moveCamera(new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()),15f);
+
+                        }else{
+                            Log.d(TAG, "onComplete: location not found");
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage() );
+        }
+   }
+
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera: camera move to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+        Gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    private void initMap(){
+        SupportMapFragment mapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(MapActivity.this);
+    }
+
+    private void getLocationPermission(){
+        String[] permissions={Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                fine_loc)== PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    coarse_loc)==PackageManager.PERMISSION_GRANTED){
+                locPermission=true;
+                initMap();
+            }
+            else{
+                ActivityCompat.requestPermissions(this,permissions,Location_permission_request_code);
+            }
+
+
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    Location_permission_request_code);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        locPermission=false;
+
+        switch(requestCode){
+            case Location_permission_request_code:{
+                if(grantResults.length>0){
+                    for (int i = 0; i < grantResults.length ; i++) {
+                        if(grantResults[i] !=PackageManager.PERMISSION_GRANTED){
+                            locPermission=false;
+                            return;
+                        }
+                    }
+                    locPermission=true;
+                    //init map
+                    initMap();
+                }
+            }
+        }
+    }
+
+
+}
